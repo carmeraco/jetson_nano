c90f0c1541a97fc87668a73958b5e93726f857a0
Justin Day <justin@jplt.com>
2019-10-09 12:00:38 -0400

    Got build to work after some fiddling

--- docker_example/build.sh
+++ docker_example/build.sh
@@ -3,7 +3,7 @@
 TAG=python_with_tensorflow
 UBUNTU_MIRROR=jp.archive.ubuntu.com
 
-docker build \
+sudo nvidia-docker build \
 	--build-arg MIRROR=$UBUNTU_MIRROR \
 	-t $TAG \
 	.
--- docker_example/run.sh
+++ docker_example/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-DOCKER_USER=foo
+DOCKER_USER=root
 
-docker run -it --rm \
- -v $PWD/home:/home/$DOCKER_USER \
+sudo nvidia-docker run -it --rm \
+ -v $PWD/home:/root \
  -v /usr/local/cuda-10.0:/usr/local/cuda-10.0 \
  -v /usr/lib/aarch64-linux-gnu/tegra:/usr/lib/aarch64-linux-gnu/tegra \
  -v /usr/lib/aarch64-linux-gnu/libcudnn.so.7.3.1:/usr/lib/aarch64-linux-gnu/libcudnn.so.7.3.1 \
